0a29a8344b9c50cd6fd442cd33f9cb2ee3d32c2e
hamid <h.khosravian@roundtableapps.com>
2017-08-29 10:22:02 +0430

    add tests

--- app/controllers/api/v1/relationship_controller.rb
+++ app/controllers/api/v1/relationship_controller.rb
@@ -7,15 +7,16 @@ module Api
         profile = User.find(params[:user_id]).profile
         unless current_user.id.eql?(params[:user_id].to_i)
           current_user.profile.follow(profile.id)
-          render json: {response: "user followed", status: 201}, status: 201
+          render json: {response: "user followed", status: 200}, status: 200
         end
       end
 
       def destroy
-        profile = Relationship.find_by(follower_id: current_user.profile.id).followed
-        unless current_user.id.eql?(params[:user_id].to_i)
+        relationship = Relationship.find_by(follower_id: current_user.profile.id)
+        if relationship 
+          profile = relationship.followed
           current_user.profile.unfollow(profile.id)
-          render json: {response: "user un-followed", status: 204}, status: 204
+          render json: {response: "user unfollowed", status: 204}, status: 204
         end
       end
     end
